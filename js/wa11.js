55d9fef432b485a50f1ba7df95f7a0eae8f5c653
emuncy <122850265+emuncy@users.noreply.github.com>
2023-04-06 13:35:42 -0600

    wa11 cont

--- js/wa11.js
+++ js/wa11.js
@@ -1,13 +1,16 @@
 const button = document.querySelector('#js-new-quote');
 button.addEventListener('click', getQuote);
 
-//const answerButton = document.querySelector('#js-tweet');
-//answerButton.addEventListener('click', showAnswer);
 
-const endpoint = 'https://evilinsult.com/generate_insult.php?lang=en&type=json';
+const answerButton = document.querySelector('#js-tweet');
+answerButton.addEventListener('click', getColors);
+
+const endpoint = 'https://uselessfacts.jsph.pl/api/v2/facts/random?language=en&type=json';
+
+const endpoint2 = 'http://colormind.io/api/model=default'
 
 async function getQuote() {
-    console.log('It works!');
+    // console.log('It works!');
     try {
         const response = await fetch(endpoint);
         if (!response.ok) {
@@ -15,8 +18,26 @@ async function getQuote() {
         }
         const json = await response.json();
         // console.log(json.question);
+        // console.log(json);
+        displayQuote(json.text);
+        
+    }
+    catch (err) {
+        console.log(err);
+        alert('Failed to fetch new trivia');
+    }
+}
+
+async function getColors() {
+    console.log('It works!');
+    try {
+        const response = await fetch(endpoint2);
+        if (!response.ok) {
+            throw Error(response.statusText)
+        }
+        const json = await response.json();
         console.log(json);
-        displayQuote(json);
+        
         
     }
     catch (err) {
@@ -28,4 +49,5 @@ async function getQuote() {
 function displayQuote(quote) {
     const quoteText = document.querySelector("#js-quote-text");
     quoteText.textContent = quote;
+    // console.log('test')
 }
